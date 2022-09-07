8f6de5a20c6e15e364b1951c4019ea7951fa7b76
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-09-07 14:45:57 +0500

    commit

---
+++ constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color orangeColor = Colors.deepOrange;
+const Color blueColor = Colors.blue;
+const headingTextStyle = TextStyle(
+  fontFamily: "Horizon",
+  fontSize: 40,
+  color: orangeColor,
+);
